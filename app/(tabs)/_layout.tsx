7efff9835e612b7e3ddc361eacfb3b6c89a9ed18
dennis <ssembatyadennis25@gmail.com>
2025-04-04 16:34:03 +0300

    style(Index): change the margin bottom of the tabBar component.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,7 +41,7 @@ const _Layout = () => {
           backgroundColor: "#0f0D23",
           borderRadius: 50,
           marginHorizontal: 20,
-          marginBottom: 36,
+          marginBottom: 15,
           height: 52,
           position: "absolute",
           overflow: "hidden",
