83c9c8a1dac8a0cfae3b5ff2367f8df223e6e5fc
dennis <ssembatyadennis25@gmail.com>
2025-04-04 10:16:46 +0300

    feat(_Layout): Add the Layout route in the (tabs) directory.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const _Layout = () => {
+  return (
+    <View>
+      <Text>Layout</Text>
+    </View>
+  );
+};
+
+export default _Layout;
+
+const styles = StyleSheet.create({});
