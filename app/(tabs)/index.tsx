583d0aa5a3f5ebfcc7832483c8aba9fe0855c90b
dennis <ssembatyadennis25@gmail.com>
2025-04-04 16:36:11 +0300

    feat(Index): Add a ScrollView container to the Home page route.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { images } from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { icons } from "@/constants/icons";
 
 export default function Index() {
   return (
@@ -8,7 +9,18 @@ export default function Index() {
       <StatusBar hidden />
       <View className="flex-1 bg-primary ">
         <View style={{ flex: 1 }}>
-          <Image source={images.bg} />
+          <Image source={images.bg} className="absolute w-full z-0" />
+
+          <ScrollView
+            className="flex-1 px-5"
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+          >
+            <Image
+              source={icons.logo}
+              className="w-12 h-10 mt-10 mb-5 mx-auto"
+            />
+          </ScrollView>
         </View>
       </View>
     </>
