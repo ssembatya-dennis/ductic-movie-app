2e1421139996bd21e940f7167d8cf5646ea4ba6a
dennis <ssembatyadennis25@gmail.com>
2025-04-06 07:04:26 +0300

    feat(Index): Render the movie Card component in the Home Screen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,6 +13,7 @@ import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
   const router = useRouter();
@@ -62,9 +63,7 @@ export default function Index() {
 
                   <FlatList
                     data={movies}
-                    renderItem={({ item }) => (
-                      <Text className="text-white text-sm">{item.title}</Text>
-                    )}
+                    renderItem={({ item }) => <MovieCard {...item} />}
                     keyExtractor={(item) => item.id.toString()}
                     numColumns={3}
                     columnWrapperStyle={{
