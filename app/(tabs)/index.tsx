91e4056a374ae64aa1f331255bcddc8cecf8ed5e
dennis <ssembatyadennis25@gmail.com>
2025-04-12 20:55:04 +0300

    feat(Index): Intergrate the Trending Card Component in the Index file.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,9 +14,17 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -41,14 +49,16 @@ export default function Index() {
               className="w-12 h-10 mt-10 mb-5 mx-auto"
             />
 
-            {moviesLoading ? (
+            {moviesLoading || trendingLoading ? (
               <ActivityIndicator
                 size="large"
                 color="#0000ff"
                 className="mt-10 self-center"
               />
-            ) : moviesError ? (
-              <Text>Error: {moviesError?.message}</Text>
+            ) : moviesError || trendingError ? (
+              <Text>
+                Error: {moviesError?.message || trendingError?.message}
+              </Text>
             ) : (
               <View className="flex-1 mt-5">
                 <SearchBar
@@ -56,7 +66,29 @@ export default function Index() {
                   placeholder="Search for a movie"
                 />
 
+                {trendingMovies && (
+                  <View className="mt-10">
+                    <Text className="text-lg text-white font-bold mb-3">
+                      Trending Movies
+                    </Text>
+                  </View>
+                )}
+
                 <>
+                  <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    ItemSeparatorComponent={() => <View className="w-4" />}
+                    className="mb-4 mt-3"
+                    data={trendingMovies}
+                    renderItem={({ item, index }) => (
+                      <>
+                        <TrendingCard movie={item} index={index} />
+                      </>
+                    )}
+                    keyExtractor={(item) => item.movie_id.toString()}
+                  />
+
                   <Text className="text-lg text-white font-bold mt-5 mb-3">
                     Latest Movies
                   </Text>
